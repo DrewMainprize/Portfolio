372585ad6355d0324d09320a7a47149ac54461f9
Drew Mainprize <drewmainprize@Drews-MacBook-Pro.local>
2021-12-24 01:37:34 -0500

    Version 1

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function(){
-    console.log("Listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
