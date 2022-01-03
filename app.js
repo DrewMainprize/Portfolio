3f1245239893626b971238c967cf2fea279a9c67
Drew Mainprize <drewmainprize@Drews-MacBook-Pro.local>
2022-01-03 13:54:49 -0500

    Fixed particles.js on landing page, updated icons on projects

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/contact", function(req, res){
     res.render("contact");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
